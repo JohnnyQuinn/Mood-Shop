fca0d40f322e4e678f37af024d6b2da5d2806070
johnnyquinn <jftq97@gmail.com>
2020-09-23 21:26:12 -0400

    styled items displayed using grid css

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -29,7 +29,6 @@ for (let i=0; i<data.length; ++i) {
     // creates a custom attribute called data-price. That will hold price for each element in the button
     button.dataset.price = data[i].price
     button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
 
 
     // Add the image to the div
@@ -39,5 +38,6 @@ for (let i=0; i<data.length; ++i) {
     newDiv.appendChild(desc)
     //add price to div
     newDiv.appendChild(price)
+    newDiv.appendChild(button)
     itemsContainer.appendChild(newDiv)
 }
